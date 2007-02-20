abb4b06518ea64a693fc9d56910fa672d6372eb1
davidbar <davidbar@3774bd98-f447-0410-8733-1d6108554824>
2007-02-20 13:02:38 +0000

    Added a QA package and a test for AlbumData. AbstractDataObject now adds an impl for hashcode()
    git-svn-id: https://tanuki2.googlecode.com/svn/trunk@17 3774bd98-f447-0410-8733-1d6108554824

--- src/golly/tanuki2/data/AbstractDataObject.java
+++ src/golly/tanuki2/data/AbstractDataObject.java
@@ -7,11 +7,11 @@ import golly.tanuki2.support.Helpers;
  * @since 19/02/2007
  */
 abstract class AbstractDataObject {
-	private String toStringCache_noId= null;
-	private String toStringCache_withId= null;
+	private String toStringCache= null;
+	private int hashcode= 0;
 
 	protected void dataUpdated() {
-		toStringCache_noId= toStringCache_withId= null;
+		toStringCache= null;
 	}
 
 	@Override
@@ -19,19 +19,22 @@ abstract class AbstractDataObject {
 		if (obj == null || !(obj instanceof AbstractDataObject))
 			return false;
 		AbstractDataObject o2= (AbstractDataObject) obj;
-		return toStringForEquals().equals(o2.toStringForEquals());
+		return toString().equals(o2.toString());
+	}
+	
+	@Override
+	public int hashCode() {
+		if (toStringCache == null)
+			toString();
+		return hashcode;
 	}
 
 	@Override
 	public String toString() {
-		if (toStringCache_withId == null)
-			toStringCache_withId= Helpers.inspect(this, true);
-		return toStringCache_withId;
-	}
-
-	private String toStringForEquals() {
-		if (toStringCache_noId == null)
-			toStringCache_noId= Helpers.inspect(this, false);
-		return toStringCache_noId;
+		if (toStringCache == null) {
+			toStringCache= Helpers.inspect(this, false);
+			hashcode= toString().intern().hashCode();
+		}
+		return toStringCache;
 	}
 }
---
+++ src/golly/tanuki2/qa/DataTest.java
@@ -0,0 +1,69 @@
+package golly.tanuki2.qa;
+
+import golly.tanuki2.data.AlbumData;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+@SuppressWarnings("nls")
+public class DataTest extends Assert {
+
+	@Test
+	public void testAlbumData() {
+		AlbumData ad1= new AlbumData(), ad2= new AlbumData();
+		assertTrue(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() == ad2.hashCode());
+		ad1.setAlbum("album");
+		assertFalse(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() != ad2.hashCode());
+		ad1.setAlbum("abc");
+		ad1.setYear(1980);
+		ad1.setArtist("qwe");
+		ad2.setAlbum("abc");
+		ad2.setYear(1980);
+		ad2.setArtist("qwe");
+
+		assertTrue(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() == ad2.hashCode());
+		ad2.setArtist(null);
+		assertFalse(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() != ad2.hashCode());
+		ad1.setArtist(null);
+		assertTrue(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() == ad2.hashCode());
+
+		ad2.setYear(null);
+		assertFalse(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() != ad2.hashCode());
+		ad1.setYear(null);
+		assertTrue(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() == ad2.hashCode());
+
+		ad2.setAlbum(null);
+		assertFalse(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() != ad2.hashCode());
+		ad1.setAlbum(null);
+		assertTrue(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() == ad2.hashCode());
+
+		ad2.setArtist("1");
+		assertFalse(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() != ad2.hashCode());
+		ad1.setArtist("2");
+		assertFalse(ad1.equals(ad2));
+		assertTrue(ad1.hashCode() != ad2.hashCode());
+
+		Set<AlbumData> set= new HashSet<AlbumData>();
+		set.add(new AlbumData());
+		assertEquals(1, set.size());
+		set.add(new AlbumData());
+		assertEquals(1, set.size());
+		set.add(ad1);
+		assertEquals(2, set.size());
+		set.add(ad1);
+		assertEquals(2, set.size());
+	}
+}
