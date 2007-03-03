f1504f03315eb1e2ebfcf6efd2c6b86f2948a895
davidbar <davidbar@3774bd98-f447-0410-8733-1d6108554824>
2007-03-03 00:37:10 +0000

    Log initialisation, config load/saving, and error handling to Startup.
    git-svn-id: https://tanuki2.googlecode.com/svn/trunk@94 3774bd98-f447-0410-8733-1d6108554824

--- src/golly/tanuki2/Startup.java
+++ src/golly/tanuki2/Startup.java
@@ -1,12 +1,15 @@
 package golly.tanuki2;
 
-import java.util.Locale;
-
 import golly.tanuki2.core.Engine;
 import golly.tanuki2.res.TanukiImage;
+import golly.tanuki2.support.Config;
 import golly.tanuki2.support.I18n;
+import golly.tanuki2.support.Log;
+import golly.tanuki2.support.TanukiException;
 import golly.tanuki2.support.UIResourceManager;
 import golly.tanuki2.ui.AppWindow;
+
+import java.util.Locale;
 
 import org.eclipse.swt.widgets.Display;
 
@@ -24,14 +27,21 @@ public class Startup {
 		Engine engine= null;
 		try {
 
+			Log.init();
+			Log.logStartup();
 			I18n.setLocale(Locale.ENGLISH);
+			Config.load();
 			display= new Display();
 			TanukiImage.setDisplay(display);
 			engine= new Engine();
 			engine.addFolder("X:\\music\\1. Fresh\\IN FLAMES Discografia (www.heavytorrents.org)");// DELME
 			new AppWindow(display, engine).show();
 
+		} catch (Throwable t) {
+			new TanukiException(t).showErrorDialog();
+
 		} finally {
+			Config.tryToSave();
 			UIResourceManager.disposeAll();
 			if (display != null)
 				display.dispose();
