590218872f03b068f7f3a5ede73cb0ff02b6c883
davidbar <davidbar@3774bd98-f447-0410-8733-1d6108554824>
2007-07-20 12:24:49 +0000

    Added some javadoc
    git-svn-id: https://tanuki2.googlecode.com/svn/trunk@241 3774bd98-f447-0410-8733-1d6108554824

--- src/golly/tanuki2/core/ITrackPropertyReader.java
+++ src/golly/tanuki2/core/ITrackPropertyReader.java
@@ -12,8 +12,21 @@ import java.util.Map;
  */
 public interface ITrackPropertyReader {
 
+	/**
+	 * Returns a map of possible {@link TrackPropertyMap}s read for each audio file contained in the given
+	 * {@link DirData}.
+	 * 
+	 * @param dd A directory worth of files.
+	 * @return A map. This cannot be <code>null</code> nor can it contain any <code>null</code> keys or values.
+	 */
 	public abstract Map<String, List<TrackPropertyMap>> readMultipleTrackProperties(final DirData dd);
 
+	/**
+	 * Reads and returns a list of possible {@link TrackPropertyMap}s for a given file.
+	 * 
+	 * @param filename The filename of the target file.
+	 * @return A list. This cannot be <code>null</code> nor can it contain any <code>null</code>s.
+	 */
 	public abstract List<TrackPropertyMap> readTrackProperties(final String filename);
 
 }
